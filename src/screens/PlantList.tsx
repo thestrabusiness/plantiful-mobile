558773ea3aeb18b93efa226972eeed12c897a419
Anthony M <anthony.moffa@thoughtbot.com>
2019-10-17 13:47:04 -0400

    add animated drawer to plant list

--- src/screens/PlantList.tsx
+++ src/screens/PlantList.tsx
@@ -1,5 +1,13 @@
-import React, { ReactElement, useState } from "react";
-import { StyleSheet, View, FlatList, Button, Text } from "react-native";
+import React, { ReactElement, useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Button,
+  Text,
+  Animated,
+  Dimensions,
+} from "react-native";
 import { Page } from "../components/Page";
 import PlantListItem from "../components/PlantListItem";
 import { onSignOut, retrieveCurrentUser } from "../Session";
@@ -11,15 +19,53 @@ interface Props {
   navigation: NavigationStackProp;
 }
 
+const windowWidth = Dimensions.get("window").width;
+const drawerWidth = 300;
+
 const styles = StyleSheet.create({
   plantList: {
     alignItems: "center",
+    width: windowWidth,
+  },
+  drawer: {
+    position: "absolute",
+    top: 0,
+    right: windowWidth,
+    bottom: 0,
+    width: drawerWidth,
+    flexDirection: "column",
+  },
+  drawerContainer: {
+    flex: 1,
+    backgroundColor: "#f53b3b",
+    alignItems: "center",
+  },
+  menuLayout: {
+    marginBottom: 1,
+    width: "100%",
+    fontSize: 25,
+    padding: 10,
   },
 });
 
 const PlantList = (props: Props): ReactElement => {
-  const [garden, setGarden] = useState<Garden>();
   const { navigation } = props;
+  const [garden, setGarden] = useState<Garden>();
+  const viewPosition = useRef(new Animated.Value(0)).current;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(viewPosition, {
+      toValue: menuOpen ? 0 : 1,
+      duration: 250,
+      useNativeDriver: true,
+    }).start();
+  }, [menuOpen]);
+
+  const viewTranslationX = viewPosition.interpolate({
+    inputRange: [0, 1],
+    outputRange: [drawerWidth, 0],
+  });
 
   retrieveCurrentUser().then((user: User | void): void => {
     if (user) {
@@ -43,24 +89,47 @@ const PlantList = (props: Props): ReactElement => {
   if (garden && garden.plants.length > 0) {
     return (
       <Page>
-        <Button
-          title="Sign Out"
-          onPress={(): void => {
-            onSignOut().then((): boolean => navigation.navigate("SignedOut"));
-          }}
-        />
-        <View style={styles.plantList}>
-          <FlatList
-            contentInsetAdjustmentBehavior="automatic"
-            data={garden.plants}
-            renderItem={({ item }): ReactElement => (
-              <PlantListItem plant={item} />
-            )}
-            keyExtractor={(item): string => item.id.toString()}
-            numColumns={3}
-            columnWrapperStyle={styles.plantList}
-          />
-        </View>
+        <Animated.View
+          style={[{ transform: [{ translateX: viewTranslationX }] }]}
+        >
+          <View style={styles.plantList}>
+            <FlatList
+              contentInsetAdjustmentBehavior="automatic"
+              data={garden.plants}
+              renderItem={({ item }): ReactElement => (
+                <PlantListItem plant={item} />
+              )}
+              keyExtractor={(item): string => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={styles.plantList}
+            />
+            <Button
+              title="Open Menu"
+              onPress={(): void => {
+                setMenuOpen(!menuOpen);
+              }}
+            />
+            <Button
+              title="Sign Out"
+              onPress={(): void => {
+                onSignOut().then((): boolean =>
+                  navigation.navigate("SignedOut"),
+                );
+              }}
+            />
+          </View>
+          <View style={styles.drawer}>
+            <Text style={styles.menuLayout}>This is the drawer</Text>
+            <Text style={styles.menuLayout}>This is menu option</Text>
+            <Text style={styles.menuLayout}>Another menu item</Text>
+            <Button
+              title="Close menu"
+              onPress={(): void => {
+                setMenuOpen(!menuOpen);
+              }}
+            />
+          </View>
+        </Animated.View>
       </Page>
     );
   } else if (garden && garden.plants.length == 0) {
@@ -68,7 +137,8 @@ const PlantList = (props: Props): ReactElement => {
       <Page>
         <View>
           <Text>
-            You don't have any plants yet! Click the + to add some new friends.
+            There are no plants in this garden! Click the + to add some new
+            friends.
           </Text>
           <Button
             title="Sign Out"
