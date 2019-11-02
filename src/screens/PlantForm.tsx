60a0513f5b7783771cc5c677b62edc5982afe7bf
Anthony M <anthony.moffa@thoughtbot.com>
2019-11-02 17:54:28 -0400

    start adding inputs

--- src/screens/PlantForm.tsx
+++ src/screens/PlantForm.tsx
@@ -1,14 +1,76 @@
 import React, { ReactElement, useState } from "react";
-import { View, Button, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Picker,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import NumericInput from "react-native-numeric-input";
 import { Page } from "../components/Page";
-import { NavigationStackProp } from "react-navigation-stack";
 
-interface Props {
-  navigation: NavigationStackProp;
-}
+const styles = StyleSheet.create({
+  inputField: {
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 10,
+    width: "100%",
+    textAlign: "center",
+  },
+});
 
-const PlantForm = () => {
-  return <Text> Add a plant!</Text>;
+const capitalize = (string: string): string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const PlantForm = (): ReactElement => {
+  const [name, setName] = useState("");
+  const [checkFrequencyScalar, setCheckFrequencyScalar] = useState(1);
+  const [checkFrequencyUnit, setCheckFrequencyUnit] = useState("day");
+
+  return (
+    <Page>
+      <Text> Add a plant!</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder={"Name"}
+        value={name}
+        onChangeText={(value: string): void => {
+          setName(value);
+        }}
+      />
+      <View style={{ flexDirection: "row" }}>
+        <NumericInput
+          value={checkFrequencyScalar}
+          onChange={(value: number): void => {
+            setCheckFrequencyScalar(value);
+          }}
+          type="plus-minus"
+        />
+        <TouchableOpacity style={{ flex: 3 }}>
+          <Text>{capitalize(checkFrequencyUnit)}</Text>
+        </TouchableOpacity>
+      </View>
+      <Picker
+        selectedValue={checkFrequencyUnit}
+        onValueChange={(value: string): void => {
+          setCheckFrequencyUnit(value);
+        }}
+      >
+        <Picker.Item label="Day" value="day" />
+        <Picker.Item label="Week" value="week" />
+      </Picker>
+      <Button
+        title="Submit"
+        onPress={(): void => {
+          console.log(name, checkFrequencyUnit, checkFrequencyScalar);
+        }}
+      />
+    </Page>
+  );
 };
 
 export default PlantForm;
